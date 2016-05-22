1745ff3242afd9593512d9f60cf8f292121d90a8
Nicholas Johnson <nicholas@forwardadvance.com>
2016-05-22 16:19:17 +0100

    exercise answers

--- exercise_answers/app/app.js
+++ exercise_answers/app/app.js
@@ -16,13 +16,25 @@ var AppComponent = ng.core
       <div>
         Weeks in a life: {{365.25 / 7 * lifespan}}
       </div>
+      <div>
+        Years until we can construct Empire State building from cats: {{yearsUntilCatsWeigh(331122430)}}
+      </div>
+      <div>
+        Years until we can replace Earth with cats: {{yearsUntilCatsWeigh(5972000000000000000000000)}}
+      </div>
     `
   })
   .Class({
     constructor: function() {
       this.lifespan = 80;
-      this.isAlive = function isAlive() {
-        return this.catAge < 15;
+      this.yearsUntilCatsWeigh = function(goal) {
+        var weight = 1;
+        var year = 0;
+        while (weight < goal) {
+          year ++;
+          weight *=6;
+        }
+        return year;
       };
     }
   });
@@ -31,7 +43,7 @@ var AppComponent = ng.core
 
 
 document.addEventListener('DOMContentLoaded', function() {
-  ng.platform.browser.bootstrap(AppComponent, [])
+  ng.platform.browser.bootstrap(AppComponent, [])''
 });
 
 
