3730fd2b08343ea8659a0e9d25e3a69f29af3166
Nicholas Johnson <nicholas@forwardadvance.com>
2016-11-10 15:54:16 +0000

    Update to Angular RC

--- demo/app/app.js
+++ demo/app/app.js
@@ -6,20 +6,44 @@
 // bind to a function on the class
 // functions should not have side effects
 
+var AppComponent = ng.core
+  .Component({
+    selector: "app",
+    template:`
+      {{catName}}
+      <div>
+        age: {{catAge}}
+      </div>
+      <div>
+        alive: {{isAlive()}}
+      </div>
+    `
+  })
+  .Class({
+    constructor: function() {
+      this.catName = "Molly";
+      this.catAge = 12;
+      this.isAlive = function isAlive() {
+        return this.catAge < 15;
+      };
+    }
+  });
 
-// Initial State
-// var AppComponent = ng.core
-//   .Component({
-//     selector: "app",
-//     template:``
-//   })
-//   .Class({
-//     constructor: function() {
-//     }
-//   });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   ng.platform.browser.bootstrap(AppComponent, [])
-// });
+var AppModule =
+  ng.core.NgModule({
+    imports: [ ng.platformBrowser.BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap: [ AppComponent ]
+  })
+  .Class({
+    constructor: function() {}
+  });
+
+
+// Finally bootstrap
+ng.platformBrowserDynamic
+  .platformBrowserDynamic()
+  .bootstrapModule(AppModule);
 
 
